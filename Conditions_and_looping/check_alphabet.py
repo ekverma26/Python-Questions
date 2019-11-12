4f91f635b9c481f052a0e89e2177bdefef24a3d0
Ekta Verma <46107362+ekverma26@users.noreply.github.com>
2019-11-12 22:45:58 +0530

    Create check_alphabet.py

---
+++ Conditions_and_looping/check_alphabet.py
@@ -0,0 +1,8 @@
+Q7 Write a Python program to check whether an alphabet is a vowel or consonant.
+
+
+alph=input(print("Enter an alphbet:"))
+if alph=="a" or alph=="e" or alph=="i" or alph=="o" or alph=="u":
+    print("Vowel")
+else:
+    print("Consonant")
