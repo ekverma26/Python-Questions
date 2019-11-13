e5cc08cffdf4dc92363ab267cc28376f0be1d847
Ekta Verma <46107362+ekverma26@users.noreply.github.com>
2019-11-13 22:14:11 +0530

    Create Table.py

---
+++ Conditions_and_looping/Table.py
@@ -0,0 +1,6 @@
+Q Write a Python program to create the multiplication table (from 1 to 10) of a number.
+
+num=int(input(print("Enter a number:")))
+for i in range(1,11,1):
+    n=num*i
+    print(n)
