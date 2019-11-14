9ae7039e707c4fb4af07c50b942859d37b82c194
Ekta Verma <46107362+ekverma26@users.noreply.github.com>
2019-11-14 22:55:34 +0530

    Create Seperate.py

---
+++ String/Seperate.py
@@ -0,0 +1,5 @@
+Q Write a Python program to get a single string from two given strings, separated by a space and swap the first two characters of each string.
+
+str1=input("Enter 1st string:")
+str2=input("Enter 2nd string:")
+print(str1[2:],str2[2:])
